069405992ba485cf225e6372cf72beea7ef1a876
Courdevelops <courdevelops@gmail.com>
2023-04-20 14:58:04 -0400

    debug path markdown needed

--- routes/articles.js
+++ routes/articles.js
@@ -12,7 +12,7 @@ router.get('/:id', (req, res) => {
 
 // saves article to database
 router.post('/', async (req, res) => {
-    const article = new Article({
+    let article = new Article({
         title: req.body.title,
         description: req.body.description,
         markdown: req.body.markdown
@@ -21,6 +21,7 @@ router.post('/', async (req, res) => {
         article = await article.save()
         res.redirect(`/articles/${article.id}`)
     } catch (e) {
+        console.log (e);
         res.render('articles/new', { article: article })
     }
 });
